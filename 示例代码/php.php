01ad2ce9487acb4e74f441f696495b3550d1ce9f
xiaoyi <38256442+xing61@users.noreply.github.com>
2023-08-16 15:20:31 +0800

    Update php.php

--- 示例代码/php.php
+++ 示例代码/php.php
@@ -16,3 +16,54 @@ $params['user'] = '张三';
 $cburl = 'http://flag.smarttrot.com/index.php/api/v1/chat/completions';
 $chatgpt_resp = Tool::_request('post', $cburl, $params, $headers);
 $data = json_decode($chatgpt_resp, true);
+
+其中发送请求的Tool
+方法如下：
+/**
+     * 发送请求，包括请求头的
+     * @param  string $method 'get'|'post' 请求的方式
+     * @param  string $url URL
+     * @param  array|json $data post请求需要发送的数据
+     * @param  bool $ssl
+     */
+    public static function _request($method='get',$url,$data=array(),$headerArray=array(),$ssl=false){
+        //初始化一个curl资源
+        $curl = curl_init();
+        //设置curl选项
+        curl_setopt($curl,CURLOPT_URL,$url);//url
+        //请求的代理信息
+        $user_agent = isset($_SERVER['HTTP_USER_AGENT'])?$_SERVER['HTTP_USER_AGENT']: 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0 FirePHP/0.7.4';
+        curl_setopt($curl,CURLOPT_USERAGENT,$user_agent);
+        curl_setopt($curl,CURLOPT_HTTPHEADER, $headerArray);
+        //referer头，请求来源
+        curl_setopt($curl,CURLOPT_AUTOREFERER,true);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 300);//设置超时时间
+        //SSL相关
+        if($ssl){
+            //禁用后，curl将终止从服务端进行验证;
+            curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,false);
+            //检查服务器SSL证书是否存在一个公用名
+            curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,2);
+        }
+        //判断请求方式post还是get
+        if(strtolower($method)=='post') {
+            /**************处理post相关选项******************/
+            //是否为post请求 ,处理请求数据
+            curl_setopt($curl,CURLOPT_POST,true);
+            curl_setopt($curl,CURLOPT_POSTFIELDS,is_array($data)?http_build_query($data):$data);
+        }
+        //是否处理响应头
+        curl_setopt($curl,CURLOPT_HEADER,false);
+        //是否返回响应结果
+        curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+
+        //发出请求
+        $response = curl_exec($curl);
+        if (false === $response) {
+            echo '<br>', curl_error($curl), '<br>';
+            return false;
+        }
+        //关闭curl
+        curl_close($curl);
+        return $response;
+    }
